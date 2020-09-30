74686e00881d535e58356d7a47d24ff915e7956a
atlas-ark <lambda.py@gmail.com>
2020-09-30 22:56:51 +0200

    :lock: Only admins mode, small fix+comment

--- index.php
+++ index.php
@@ -5,12 +5,16 @@ include 'constants.php';
 include 'header.php';
 //This file contains all base functions
 include 'basefunctions.php';
-//This file contains logger code and variables
-include 'logger.php';
 //This file contains all global variables from the various update types
 include 'variables.php';
-//Checks if user is an admin, otherwise exits
+/** Checks if user is an admin, otherwise exits
+ * @todo Update with a better solution (query)
+ * @todo Fix include order
+ * @todo Create one or more functions to check if a user is admin and related permissions
+ */
 if (!in_array($userid,ADMINS)) exit;
+//This file contains logger code and variables
+include 'logger.php';
 if ($text === "/start") {
   sendMessage($userid,"Hi!");
   exit;
