5943ef01693e5a2fbdac7ca18b612c1ddca5190d
Giulio Coa <giulioc008@gmail.com>
2020-10-02 08:46:03 +0200

    :art: :recycle: Refactored the code and added the check to the database connection

--- header.php
+++ header.php
@@ -1,9 +1,15 @@
 <?php
-//Gets the incoming JSON update
-$content = file_get_contents("php://input");
-//Decodes update into an array
-$update = json_decode($content, true);
-/** Opens a MySQL connection. Credentials are stored in private.php file
- * @todo Check for errors
- */
-$dbconn = mysqli_connect(DBHOST, DBUSER, DBPWD, DBNAME);
+	// Gets the incoming JSON update
+	$content = file_get_contents("php://input");
+	
+	// Decodes update into an array
+	$update = json_decode($content, TRUE);
+	
+	// Opens a MySQL connection. Credentials are stored in private.php file
+	$db_conn = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
+	
+	// Checking if the connecion is failed
+	if ($db_conn -> connect_errno) {
+		exit(1);
+	}
+?>
