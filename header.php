476df39f9f963f061dfe35f6c1e71515f8d82560
Giulio Coa <giulioc008@gmail.com>
2020-10-05 16:22:41 +0200

    :recycle: :bulb: Refactoring the code and converting the code to the camelCase naming convenction

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* These are all the base functions that will be used for communicating
+* These are all the preliminary operation that will be used for communicating
 * with Telegram Bot.
 *
 * No libraries are used in this project.
@@ -17,16 +17,29 @@
 * @license		https://choosealicense.com/licenses/gpl-3.0/
 */
 
-// Gets the incoming JSON update
-$content = file_get_contents("php://input");
+/**
+* Gets the incoming JSON update
+*
+* file_get_contents() Reads the file into a string
+*/
+$content = file_get_contents('php://input');
 
-// Decodes update into an array
+/**
+* Decode the update
+*
+* json_decode() Convert the output to a PHP object
+*/
 $update = json_decode($content, TRUE);
 
-// Opens a MySQL connection. Credentials are stored in private.php file
-$db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWD, DB_NAME, DB_PORT);
+/**
+* Opens a MySQL connection.
+* Credentials are stored in private.php file
+*
+* mysqli_connect() Open the connection with the database
+*/
+$dbConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWD, DB_NAME, DB_PORT);
 
 // Checking if the connection is failed
-if ($db_conn === FALSE) {
+if ($dbConnection === FALSE) {
 	exit(1);
 }
