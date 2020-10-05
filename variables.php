3161469900fdf6c1e64bb3d0480038d92889b29b
Giulio Coa <giulioc008@gmail.com>
2020-10-05 16:15:07 +0200

    :recycle: :bulb: Refactoring the code

--- variables.php
+++ variables.php
@@ -1,7 +1,11 @@
 <?php
 /**
-* These are all the base functions that will be used for communicating
-* with Telegram Bot.
+* This section of the code defines all required variables
+* by getting information from Telegram Bot's update.
+*
+* @todo Adapt to our needs
+* @todo Not the best way to proceed, use another method
+* @todo Check for undefined variables with empty()
 *
 * No libraries are used in this project.
 *
@@ -18,33 +22,129 @@
 */
 
 /**
-* This section of the code defines all required variables
-* by getting information from Telegram Bot's update.
+* Check if the message has a text
 *
-* @todo Adapt to our needs
-* @todo Not the best way to proceed, use another method
-* @todo Check for undefined variables with empty()
+* empty() check if the argument is empty
+* 	''
+* 	""
+* 	'0'
+* 	"0"
+* 	0
+* 	0.0
+* 	NULL
+* 	FALSE
+* 	[]
+* 	array()
 */
+$text = empty($update['message']['text']) === FALSE ? trim($update['message']['text']) : '';
+$callbackData = $update['callback_query']['data'];
+$messageId = $update['callback_query']['message']['message_id'];
+$callbackId = $update['callback_query']['id'];
+$inlineQueryId = $update['inline_query']['id'];
+$inlineQuery = $update['inline_query']['query'];
 
-$text = empty($update['message']['text']) === FALSE ? trim($update['message']['text']) : "";
-$callback_data = $update['callback_query']['data'];
-$msg_id = $update['callback_query']['message']['message_id'];
-$callback_id = $update['callback_query']['id'];
-$inline_query_id = $update["inline_query"]["id"];
-$inline_query = $update["inline_query"]["query"];
-
+/**
+* Check if the message is a CallbackQuery
+*
+* empty() check if the argument is empty
+* 	''
+* 	""
+* 	'0'
+* 	"0"
+* 	0
+* 	0.0
+* 	NULL
+* 	FALSE
+* 	[]
+* 	array()
+*/
 if (empty($update['callback_query']) === FALSE) {
 	$user = $update['callback_query']['from'];
+/**
+* Check if the message is a InlineQuery
+*
+* empty() check if the argument is empty
+* 	''
+* 	""
+* 	'0'
+* 	"0"
+* 	0
+* 	0.0
+* 	NULL
+* 	FALSE
+* 	[]
+* 	array()
+*/
 } else if (empty($update['inline_query']) === FALSE) {
 	$user = $update['inline_query']['from'];
+/**
+* Check if the message is a text message
+*
+* empty() check if the argument is empty
+* 	''
+* 	""
+* 	'0'
+* 	"0"
+* 	0
+* 	0.0
+* 	NULL
+* 	FALSE
+* 	[]
+* 	array()
+*/
 } else if (empty($update['message']) === FALSE) {
 	$user = $update['message']['from'];
 }
 
-$user_id = $user['id'];
-$first_name = $user['first_name'];
-$last_name = empty($user['last_name']) === FALSE ? $user['last_name'] : "";
-$username = empty($user['username']) === FALSE ? $user['username'] : "";
-$lang = empty($user['language_code']) === FALSE ? $user['language_code'] : "";
+$userId = $user['id'];
+$firstName = $user['first_name'];
+/**
+* Check if the user has a last name
+*
+* empty() check if the argument is empty
+* 	''
+* 	""
+* 	'0'
+* 	"0"
+* 	0
+* 	0.0
+* 	NULL
+* 	FALSE
+* 	[]
+* 	array()
+*/
+$lastName = empty($user['last_name']) === FALSE ? $user['last_name'] : '';
+/**
+* Check if the user has a username
+*
+* empty() check if the argument is empty
+* 	''
+* 	""
+* 	'0'
+* 	"0"
+* 	0
+* 	0.0
+* 	NULL
+* 	FALSE
+* 	[]
+* 	array()
+*/
+$username = empty($user['username']) === FALSE ? $user['username'] : '';
+/**
+* Check if the user has a language code
+*
+* empty() check if the argument is empty
+* 	''
+* 	""
+* 	'0'
+* 	"0"
+* 	0
+* 	0.0
+* 	NULL
+* 	FALSE
+* 	[]
+* 	array()
+*/
+$lang = empty($user['language_code']) === FALSE ? $user['language_code'] : '';
 
-$name_text = $first_name . " " . $last_name;
+$completeName = $firstName . ' ' . $lastName;
